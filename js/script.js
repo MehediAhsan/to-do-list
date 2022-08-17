76f70659343af291f9e07b5c9e206b0440f27caa
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-18 00:33:31 +0600

    bug fixed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let count = 0;
 document.getElementById("input-btn").addEventListener("click", function () {
   count++;
   const inputValue = document.getElementById("input-value").value;
+  if(inputValue === ""){
+    alert("Please add some task here");
+    return;
+  }
 
   const contentContainer = document.getElementById("content-container");
   const tableBodyRow = document.createElement("tr");
@@ -9,8 +13,8 @@ document.getElementById("input-btn").addEventListener("click", function () {
   <th scope="row">${count}</th>
   <td>${inputValue}</td>
   <td>
-  <button class="btn btn-primary delete-btn">Delete</button>
-  <button class="btn btn-secondary done-btn">Done</button>
+  <button class="btn btn-danger delete-btn">Delete</button>
+  <button class="btn btn-success done-btn">Done</button>
   </td>
   `;
   contentContainer.appendChild(tableBodyRow);
@@ -24,7 +28,7 @@ document.getElementById("input-btn").addEventListener("click", function () {
   const doneBtn = document.getElementsByClassName("done-btn");
   for (let button of doneBtn) {
     button.addEventListener("click", function (event) {
-      event.target.parentNode.parentNode.style.textDecorationLine = 'line-through'; 
+      event.target.parentNode.parentNode.children[1].style.textDecorationLine = "line-through";
     });
   }
   const clearBtn = document.getElementById("clear-btn");
